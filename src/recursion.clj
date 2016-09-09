405fa6f815118384bdd594ef92cc0bacae9cc74c
Tommi Helander <tommi.helander@iki.fi>
2016-09-09 06:20:43 +0300

    Some exercises done

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,40 +1,92 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+    1
+    (* (first coll)
+       (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (and (not (empty? coll)) (empty? (rest coll))))
 
 (defn my-last [coll]
-  :-)
+  (if (empty? (rest coll))
+    (first coll)
+    (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (if (empty? a-seq)
+    nil
+    (max (first a-seq)
+         (if (nil? (max-element (rest a-seq)))
+           (- 1 (first a-seq))
+           (max-element (rest a-seq))))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (let [seq-1-count (count seq-1)
+        seq-2-count (count seq-2)]
+    (if (> seq-1-count seq-2-count)
+      seq-1
+      seq-2)))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    nil
+    (seq-max (first a-seq)
+             (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (let [current (first a-seq)]
+      (if (pred? current)
+        (cons current (my-filter pred? (rest a-seq)))
+        (my-filter pred? (rest a-seq))))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond
+    (empty? a-seq)
+      false
+    (not= elem (first a-seq))
+      (sequence-contains? elem (rest a-seq))
+    :else
+      true))
+
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq)
+      '()
+    (pred? (first a-seq))
+      (cons (first a-seq)
+            (my-take-while pred? (rest a-seq)))
+    :else
+      '()))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq)
+      '()
+    (pred? (first a-seq))
+      (my-drop-while pred? (rest a-seq))
+    :else
+      (seq a-seq)))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (cond
+    (and (empty? a-seq) (empty? b-seq))
+      true
+    (= (first a-seq) (first b-seq))
+      (seq= (rest a-seq) (rest b-seq))
+    :else
+      false))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (if (or (empty? seq-1) (empty? seq-2))
+    '()
+    (cons (f (first seq-1) (first seq-2))
+          (my-map f (rest seq-1) (rest seq-2)))))
+
 
 (defn power [n k]
   :-)
